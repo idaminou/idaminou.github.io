0b3c82aef01d9e432bbadfba43a8666b96348220
idaminou <156721838+idaminou@users.noreply.github.com>
2024-02-04 02:05:32 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+const button = document.getElementById('curumeu');
+
+var positionRan;
+var inPos = String(positionRan) + 'px';
+
+function plzz(vp) {
+    positionRan = Math.floor(Math.random() * 1000) - 50;
+    if (positionRan > vh) {
+        plzz();
+    } else if (positionRan > vw) {
+        plzz();
+    }
+    console.log(positionRan);
+
+    return String(positionRan) + 'px';
+}
+
+button.addEventListener('click', () => {
+    button.style.position = 'absolute';
+    button.style.top = plzz(vw);
+    button.style.left = plzz(vh);
+});
+
+let vw = Math.max(
+    document.documentElement.clientWidth || 0,
+    window.innerWidth || 0
+);
+let vh = Math.max(
+    document.documentElement.clientHeight || 0,
+    window.innerHeight || 0
+);
+
+button.addEventListener('click', () => {
+    console.log(vw, vh);
+});
